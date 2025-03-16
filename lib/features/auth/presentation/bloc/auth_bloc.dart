de7ba532d6fbc792418ab0b0d3f8e1de5af5a579
mohamed0yaser <mohamedyaser2023@gmail.com>
2025-03-16 22:59:25 +0200

    get the current user and done with auth

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,4 +1,7 @@
-import 'package:blog_application/features/auth/domain/entities/user.dart';
+import 'package:blog_application/core/common/cubits/app_user/app_user_cubit.dart';
+import 'package:blog_application/core/usecase/usecase.dart';
+import 'package:blog_application/core/common/entities/user.dart';
+import 'package:blog_application/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_application/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_application/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
@@ -10,19 +13,35 @@ part 'auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
   final UserLogin _userLogin;
+  final CurrentUser _currentUser;
+  final AppUserCubit _appUserCubit;
   AuthBloc({
     required UserLogin userLogin,
     required UserSignUp userSignUp,
+    required CurrentUser currentUser,
+    required AppUserCubit appUserCubit,
   })  : _userSignUp = userSignUp,
-  _userLogin = userLogin,
+        _userLogin = userLogin,
+        _currentUser = currentUser,
+        _appUserCubit = appUserCubit,
         super(AuthInitial()) {
-    on<AuthSignUp>(_onAuthSignup);
-    on<AuthLogin>(_onAuthLogin);
-    
+          on<AuthEvent>((_, emit) {emit(AuthLoading());});
+          on<AuthSignUp>(_onAuthSignup);
+          on<AuthLogin>(_onAuthLogin);
+          on<AuthCurrentUserLoggedIn>(_onAuthCurrentUserLoggedIn);
+          
   }
 
-  void _onAuthSignup(AuthSignUp event, Emitter emit) async {
-    emit(AuthLoading());
+  void _onAuthCurrentUserLoggedIn(AuthCurrentUserLoggedIn event, Emitter<AuthState> emit) async {
+    final results = await _currentUser(NoParams());
+    results.fold(
+      (failure) => emit(AuthFailure(message: failure.message)),
+      (user) => _emitAuthSuccess(user, emit),
+    );
+  }
+
+  void _onAuthSignup(AuthSignUp event, Emitter<AuthState> emit) async {
+
     final results = await _userSignUp(
       UserSignUpParams(
         name: event.name,
@@ -33,13 +52,12 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
     results.fold(
       (failure) => emit(AuthFailure(message: failure.message)),
-      (user) => emit(AuthSuccess(user: user)),
+      (user) => _emitAuthSuccess(user, emit),
     );
-    print(results);
   }
 
-  void _onAuthLogin(AuthLogin event, Emitter emit) async {
-    emit(AuthLoading());
+  void _onAuthLogin(AuthLogin event, Emitter<AuthState> emit) async {
+ 
     final results = await _userLogin(
       UserLoginParams(
         email: event.email,
@@ -49,8 +67,11 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
     results.fold(
       (failure) => emit(AuthFailure(message: failure.message)),
-      (user) => emit(AuthSuccess(user: user)),
+      (user) => _emitAuthSuccess (user, emit),
     );
-    print(results);
+  }
+  void _emitAuthSuccess (User user, Emitter<AuthState> emit){
+    emit(AuthSuccess(user: user));
+    _appUserCubit.updateUser(user);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blog_application/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:blog_application/core/theme/theme.dart';
 import 'package:blog_application/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_application/features/auth/presentation/pages/login_page.dart';
@@ -12,23 +13,50 @@ void main() async {
   runApp(
     MultiBlocProvider(
       providers: [
-        BlocProvider(
-          create: (_) => serviceLocatot<AuthBloc>(),
-        ),
+        BlocProvider(create: (_) => serviceLocatot<AppUserCubit>()),
+        BlocProvider(create: (_) => serviceLocatot<AuthBloc>()),
       ],
       child: const MyApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthBloc>().add(AuthCurrentUserLoggedIn());
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Blog App',
-        theme: AppTheme.darkThemeMode,
-        home: const LogInPage());
+      debugShowCheckedModeBanner: false,
+      title: 'Blog App',
+      theme: AppTheme.darkThemeMode,
+      home: BlocSelector<AppUserCubit, AppUserState, bool>(
+        selector: (state) {
+          return state is AppUserLoggedIn;
+        },
+        builder: (context, isLoggedIn) {
+          if (isLoggedIn) {
+            return const Scaffold(
+              body: Center(
+                child: Text('Home Page'),
+              ),
+            );
+          }
+
+          return const LogInPage();
+        },
+      ),
+    );
   }
 }
